4d05d0812c565fc1da3a814dcb60f8f44e609da4
Chintana Wilamuna <chintanaw@gmail.com>
2015-12-24 10:43:51 -0800

    Hacking around ...

--- goscim.go
+++ goscim.go
@@ -3,207 +3,177 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
-	"os"
 	"time"
 
+	"bitbucket.org/chintana/goscim/types"
+	"bitbucket.org/chintana/goscim/util"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
-type User struct {
-	Active            bool          `json:"active"`
-	Addresses         []Address     `json:"addresses"`
-	DisplayName       string        `json:"displayName"`
-	Emails            []Email       `json:"emails"`
-	ExternalId        string        `json:"externalId"`
-	Groups            []Group       `json:"groups"`
-	Id                string        `json:"id"`
-	Ims               []Im          `json:"ims"`
-	Locale            string        `json:"locale"`
-	Meta              MetaT         `json:"meta"`
-	Name              NameT         `json:"name"`
-	NickName          string        `json:"nickName"`
-	Password          string        `json:"password"`
-	PhoneNumbers      []PhoneNumber `json:"phoneNumbers"`
-	Photos            []Photo       `json:"photos"`
-	PreferredLanguage string        `json:"preferredLanguage"`
-	ProfileUrl        string        `json:"profileUrl"`
-	Schemas           []string      `json:"schemas"`
-	TimeZone          string        `json:"timezone"`
-	Title             string        `json:"title"`
-	UserName          string        `json:"userName"`
-	UserType          string        `json:"userType"`
-	X509Certificates  []Cert        `json:"x509certificates"`
+// Add user to DB and return error status
+func (s *SCIMServer) AddUser(u *types.User) (err error) {
+	c := s.db.C("users")
+	err = c.Insert(u)
+	return err
 }
 
-type Cert struct {
-	Value string `json:"value"`
-}
-
-type Photo struct {
-	Type  string `json:"type"`
-	Value string `json:"value"`
-}
-
-type PhoneNumber struct {
-	Type  string `json:"type"`
-	Value string `json:"value"`
-}
-
-type NameT struct {
-	FamilyName      string `json:"familyName"`
-	Formatted       string `json:"formatted"`
-	GivenName       string `json:"givenName"`
-	HonorificPrefix string `json:"honorificPrefix"`
-	HonorificSuffix string `json:"honorificSuffix"`
-	MiddleName      string `json:"middleName"`
-}
-
-type MetaT struct {
-	Created      string `json:"created"`
-	LastModified string `json:"lastModified"`
-	Location     string `json:"location"`
-	ResourceType string `json:"resourceType"`
-	Version      string `json:"version"`
-}
-
-type Im struct {
-	Type  string `json:"type"`
-	Value string `json:"value"`
-}
-
-type Group struct {
-	Ref     string `json:"ref"`
-	Dispaly string `json:"display"`
-	Value   string `json:"value"`
-}
-
-type Email struct {
-	Primary bool   `json:"primary"`
-	Type    string `json:"type"`
-	Value   string `json:"value"`
-}
-
-type Address struct {
-	Country       string `json:"country"`
-	Formatted     string `json:"formatted"`
-	Locality      string `json:"locality"`
-	PostalCode    string `json:"postalCode"`
-	Primary       bool   `json:"primary"`
-	Region        string `json:"region"`
-	StreetAddress string `json:"streetAddress"`
-	Type          string `json:"type"`
-}
-
-// Save the user to DB
-func (user *User) save() (bool, error) {
-	fmt.Println("Saving user...")
-	session, err := mgo.Dial("127.0.0.1:27017")
+// Get user info
+func (s *SCIMServer) GetUser(id string) (*types.User, error) {
+	c := s.db.C("users")
+	var user *types.User
+	err := c.Find(bson.M{"id": id}).One(&user)
 	if err != nil {
-		return false, err
+		return nil, err
 	}
-	defer session.Close()
-
-	c := session.DB("test").C("users")
-	err = c.Insert(user)
-	if err != nil {
-		return false, err
-	}
-	return true, nil
+	return user, nil
 }
 
-func getUUID() string {
-	f, _ := os.OpenFile("/dev/urandom", os.O_RDONLY, 0)
-	b := make([]byte, 16)
-	f.Read(b)
-	f.Close()
-	return fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
-}
+// Create a user data structure from incoming request body, then return a User struct that
+// represent incoming user info in JSON payload
+func createUser(b io.Reader) (user *types.User, err error) {
+	user = new(types.User)
 
-// Add a user to the system
-func addUser(w http.ResponseWriter, r *http.Request) {
-	ud := json.NewDecoder(r.Body)
-	var user = new(User)
-	err := ud.Decode(&user)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+	if err = json.NewDecoder(b).Decode(&user); err != nil {
+		return nil, err
 	}
-
-	// Generate metadata before saving
-	user.Id = getUUID()
+	// Generate metadata
+	user.Id = util.GetUUID()
 	created := time.Now()
 	user.Meta.Created = created.Format(time.RFC3339)
 	user.Meta.LastModified = user.Meta.Created
 	user.Meta.Location = "http://localhost:8080/Users/" + user.Id
 
-	go user.save()
-
-	w.Header().Set("Location", "http://localhost:8080/Users/"+user.Id)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-
-	m, err := json.Marshal(user)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Write(m)
+	return user, nil
 }
 
-// Get user from DB
-func getUser(w http.ResponseWriter, r *http.Request) {
-	// get user ID
-	userId := r.RequestURI[7:]
-
-	fmt.Println("Getting user...")
-	session, err := mgo.Dial("127.0.0.1:27017")
+// Connect to the given MongoDB instance and return a pointer to the connection. DB will be
+// accessed through this pointer
+func NewMongoDS(url string) *mgo.Database {
+	fmt.Println("Connecting to MongoDB - ", url)
+	session, err := mgo.Dial(url)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		panic(err)
 	}
-	defer session.Close()
-
-	c := session.DB("test").C("users")
-
-	user := User{}
-	fmt.Println("Finding user...")
-	err = c.Find(bson.M{"id": userId}).One(&user)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Location", "http://localhost:8080/Users/"+user.Id)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-
-	m, err := json.Marshal(user)
-	fmt.Println("Marshalling user...")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Write(m)
+	return session.DB("test")
 }
 
-func userHandler(w http.ResponseWriter, r *http.Request) {
-	switch {
-	case r.Method == "GET":
-		getUser(w, r)
-	case r.Method == "POST":
-		addUser(w, r)
-	case r.Method == "PUT":
-	case r.Method == "PATCH":
-	case r.Method == "DELETE":
+// Hold data for the server instance.
+type SCIMServer struct {
+	// Pointer used to connect to a MongoDB instance
+	db *mgo.Database
+}
+
+// Handle all requests coming to /Users - GET, POST, PUT, PATCH, DELETE
+func (server *SCIMServer) userHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		log.Println("inside get")
+		// Search for user ID, then return user details in JSON
+		user, err := server.GetUser(r.URL.Path)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var data []byte
+		err = json.Unmarshal(data, user)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Location", "http://localhost:8181/Users/"+user.Id)
+		w.WriteHeader(http.StatusOK)
+		w.Write(data)
+	case "POST":
+		// Create a new resource (user)
+		// http://www.simplecloud.info/specs/draft-scim-api-01.html#create-resource
+
+		// Quote:
+		//
+		// Successful Resource creation is indicated with a 201 ("Created") response code. Upon
+		// successful creation, the response body MUST contain the newly created Resource. Since
+		// the server is free to alter and/or ignore POSTed content, returning the full
+		// representation can be useful to the client, enabling it to correlate the client and
+		// server views of the new Resource. When a Resource is created, its URI must be returned
+		// in the response Location header.
+
+		// If the Service Provider determines creation of the requested Resource conflicts with
+		// existing resources; e.g., a User Resource with a duplicate userName, the Service Provider
+		// MUST return a 409 error and SHOULD indicate the conflicting attribute(s) in the body of
+		// the response.
+
+		user, err := createUser(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		err = server.AddUser(user)
+		if err != nil {
+			// TODO: check whether it's a duplicate username and throw a 409
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		m, err := json.Marshal(user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Location", "http://localhost:8181/Users/"+user.Id)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		w.Write(m)
 	default:
-		// Any other method should result in an error
-		http.Error(w, "Unsupported HTTP method", http.StatusMethodNotAllowed)
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 	}
+}
+
+// Handle all requests coming to /Groups - GET, POST, PUT, PATCH, DELETE
+func (s *SCIMServer) groupHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+// Handle all requests coming to /ServiceProviderConfigs - GET
+func (s *SCIMServer) spConfigHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+// Handle all requests coming to /Schemas - GET
+func (s *SCIMServer) schemaHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+// Handle all requests coming to /Bulk - POST
+func (s *SCIMServer) bulkHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+func (s *SCIMServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println("inside server")
+	switch r.URL.Path {
+	case "/Users/":
+		log.Println("matched Users")
+		s.userHandler(w, r)
+	case "/Groups":
+		s.groupHandler(w, r)
+	case "/ServiceProviderConfigs":
+		s.spConfigHandler(w, r)
+	case "/Schemas":
+		s.schemaHandler(w, r)
+	case "/Bulk":
+		s.bulkHandler(w, r)
+	}
+
 }
 
 func main() {
-	http.HandleFunc("/Users/", userHandler)
-	http.ListenAndServe(":8181", nil)
+	// Initialize server environment
+	s := &SCIMServer{
+		// database instance
+		db: NewMongoDS("127.0.0.1:27017"),
+	}
+
+	http.Handle("/", s)
+	log.Println("Server started http://localhost:8181")
+	log.Fatal(http.ListenAndServe(":8181", nil))
 }
